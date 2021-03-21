44bad11b8b3df89782977bdc6dce16cab26f6d2f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-03-21 10:22:30 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by masaru tsuchiyama on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
